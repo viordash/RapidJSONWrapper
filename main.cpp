9b68bbfecf4dde2efa71070b6ef804bdb5572d7f
Vitaliy Orazov <vitaly.orazov@rubetek.com>
2022-06-19 19:15:30 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(const int argc, const char *argv[]) {
+	fprintf(stdout, "json to class object\n");
+
+	return EXIT_SUCCESS;
+}
