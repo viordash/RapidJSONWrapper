d310cf7e4ec86483499a3344bb0bbb30706b77a2
Vitaliy Orazov <viordash@mail.ru>
2022-07-17 23:27:32 +0300

    WIP. JsonArray for other types

--- Json/JsonArray.h
+++ Json/JsonArray.h
@@ -15,20 +15,35 @@ class JsonArrayBase {
 };
 
 template <class TItem> class JsonArray : public JsonArrayBase {
+	typedef typename std::remove_pointer<TItem>::type TNewObjectItem;
+
   public:
-	std::vector<TItem *> Items;
-	TItem *operator[](int index) { return Items[index]; }
+	std::vector<TItem> Items;
+	TItem operator[](int index) { return Items[index]; }
 
 	virtual bool TryParse(TJsonDocument *doc) {
 		if (!doc->IsArray()) { return false; }
 
-		for (const auto &item : doc->GetArray()) {
-			if (!TryParseItem((TJsonDocument *)&item)) { return false; }
+		if (std::is_base_of<JsonObject, TNewObjectItem>::value) {
+			return TryParseJsonObject(doc);
+		} else if (std::is_same<TItem, char *>::value) {
+			return TryParseString(doc);
+		} else if (std::is_same<TItem, bool>::value) {
+			return TryParseBool(doc);
+		} else if (std::is_same<TItem, int64_t>::value) {
+			return TryParseInt64(doc);
+		} else if (std::is_same<TItem, uint64_t>::value) {
+			return TryParseUint64(doc);
+		} else if (std::is_signed<TItem>::value) {
+			return TryParseInt(doc);
+		} else if (std::is_unsigned<TItem>::value) {
+			return TryParseUint(doc);
+		} else {
+			return false;
 		}
-		return true;
 	}
 
-	bool TryParse(const char *jsonStr, int length = -1)  {
+	bool TryParse(const char *jsonStr, int length = -1) {
 		auto doc = BeginTryParse(jsonStr, length);
 		if (doc == NULL) { return false; }
 		EndTryParse(doc);
@@ -54,12 +69,22 @@ template <class TItem> class JsonArray : public JsonArrayBase {
 	void EndTryParse(TJsonDocument *doc) { delete doc; }
 
 	virtual void WriteToDoc(TJsonDocument *doc) {
-		rapidjson::Document::AllocatorType &allocator = doc->GetAllocator();
 		doc->SetArray();
-		for (const auto item : Items) {
-			rapidjson::Document aChildDoc(&allocator);
-			item->WriteToDoc(&aChildDoc);
-			doc->PushBack(aChildDoc, allocator);
+
+		if (std::is_base_of<JsonObject, TNewObjectItem>::value) {
+			WriteJsonObjectToDoc(doc);
+		} else if (std::is_same<TItem, char *>::value) {
+			WriteJsonStringToDoc(doc);
+		} else if (std::is_same<TItem, bool>::value) {
+			WriteJsonBoolToDoc(doc);
+		} else if (std::is_same<TItem, int64_t>::value) {
+			WriteJsonInt64ToDoc(doc);
+		} else if (std::is_same<TItem, uint64_t>::value) {
+			WriteJsonUint64ToDoc(doc);
+		} else if (std::is_signed<TItem>::value) {
+			WriteJsonIntToDoc(doc);
+		} else if (std::is_unsigned<TItem>::value) {
+			WriteJsonUintToDoc(doc);
 		}
 	}
 
@@ -92,21 +117,134 @@ template <class TItem> class JsonArray : public JsonArrayBase {
 		return size;
 	}
 
-	virtual bool Add(TItem *item) {
-		if (item == NULL) { return false; }
+	virtual bool Add(TItem item) {
 		if (!Validate(item)) { return false; }
-		Items.push_back(item);
+
+		if (std::is_same<TItem, char *>::value) {
+			auto len = strlen((char *)item);
+			auto newItem = new char[len + 1];
+			memcpy(newItem, (char *)item, len);
+			newItem[len] = 0;
+			Items.push_back((TItem)newItem);
+		} else {
+			Items.push_back(item);
+		}
+
 		return true;
 	}
 
   protected:
-	virtual bool TryParseItem(TJsonDocument *doc) {
-		auto newItem = new TItem();
-		if (!newItem->TryParse(doc) || !Add(newItem)) {
-			delete newItem;
-			return false;
+	virtual bool Validate(TItem item) = 0;
+
+  private:
+	bool TryParseJsonObject(TJsonDocument *doc) {
+		for (const auto &jItem : doc->GetArray()) {
+			auto newItem = new TNewObjectItem();
+			if (!((JsonObject *)newItem)->TryParse((TJsonDocument *)&jItem) || !Add((TItem)newItem)) {
+				delete newItem;
+				return false;
+			}
 		}
 		return true;
 	}
-	virtual bool Validate(TItem *item) = 0;
+
+	bool TryParseString(TJsonDocument *doc) {
+		for (const auto &jItem : doc->GetArray()) {
+			if (!jItem.IsString() || !Add((TItem)jItem.GetString())) { return false; }
+		}
+		return true;
+	}
+
+	bool TryParseInt(TJsonDocument *doc) {
+		for (const auto &jItem : doc->GetArray()) {
+			if (!jItem.IsInt() || !Add((TItem)jItem.GetInt())) { return false; }
+		}
+		return true;
+	}
+
+	bool TryParseUint(TJsonDocument *doc) {
+		for (const auto &jItem : doc->GetArray()) {
+			if (!jItem.IsUint() || !Add((TItem)jItem.GetUint())) { return false; }
+		}
+		return true;
+	}
+
+	bool TryParseInt64(TJsonDocument *doc) {
+		for (const auto &jItem : doc->GetArray()) {
+			if (!jItem.IsInt64() || !Add((TItem)jItem.GetInt64())) { return false; }
+		}
+		return true;
+	}
+
+	bool TryParseUint64(TJsonDocument *doc) {
+		for (const auto &jItem : doc->GetArray()) {
+			if (!jItem.IsUint64() || !Add((TItem)jItem.GetUint64())) { return false; }
+		}
+		return true;
+	}
+
+	bool TryParseBool(TJsonDocument *doc) {
+		for (const auto &jItem : doc->GetArray()) {
+			if (!jItem.IsBool() || !Add((TItem)jItem.GetBool())) { return false; }
+		}
+		return true;
+	}
+
+	void WriteJsonObjectToDoc(TJsonDocument *doc) {
+		rapidjson::Document::AllocatorType &allocator = doc->GetAllocator();
+		for (const auto &item : Items) {
+			rapidjson::Document childDoc(&allocator);
+			JsonObject *jObject = (JsonObject *)item;
+			jObject->WriteToDoc(&childDoc);
+			doc->PushBack(childDoc, allocator);
+		}
+	}
+	void WriteJsonStringToDoc(TJsonDocument *doc) {
+		rapidjson::Document::AllocatorType &allocator = doc->GetAllocator();
+		for (const auto &item : Items) {
+			rapidjson::Value temp;
+			temp.SetString(rapidjson::StringRef((char *)item));
+			doc->PushBack(temp, allocator);
+		}
+	}
+	void WriteJsonIntToDoc(TJsonDocument *doc) {
+		rapidjson::Document::AllocatorType &allocator = doc->GetAllocator();
+		for (const auto &item : Items) {
+			rapidjson::Value temp;
+			temp.SetInt((signed int)item);
+			doc->PushBack(temp, allocator);
+		}
+	}
+	void WriteJsonUintToDoc(TJsonDocument *doc) {
+		rapidjson::Document::AllocatorType &allocator = doc->GetAllocator();
+		for (const auto &item : Items) {
+			rapidjson::Value temp;
+			temp.SetUint((unsigned int)item);
+			doc->PushBack(temp, allocator);
+		}
+	}
+	void WriteJsonInt64ToDoc(TJsonDocument *doc) {
+		rapidjson::Document::AllocatorType &allocator = doc->GetAllocator();
+		for (const auto &item : Items) {
+			rapidjson::Value temp;
+			temp.SetInt64((int64_t)item);
+			doc->PushBack(temp, allocator);
+		}
+	}
+	void WriteJsonUint64ToDoc(TJsonDocument *doc) {
+		rapidjson::Document::AllocatorType &allocator = doc->GetAllocator();
+		for (const auto &item : Items) {
+			rapidjson::Value temp;
+			temp.SetUint64((uint64_t)item);
+			doc->PushBack(temp, allocator);
+		}
+	}
+	void WriteJsonBoolToDoc(TJsonDocument *doc) {
+		rapidjson::Document::AllocatorType &allocator = doc->GetAllocator();
+		for (const auto &item : Items) {
+			rapidjson::Value temp;
+			temp.SetBool(item);
+			doc->PushBack(temp, allocator);
+		}
+	}
 };
--- Tests/JsonArrayTestsGroup.cpp
+++ Tests/JsonArrayTestsGroup.cpp
@@ -21,86 +21,134 @@ class UserDto : public JsonObject {
 	UserDto()
 		: Name(this, "name"), //
 		  Role(this, "role") {}
+
+	bool Validate() override { return Role.Value < 1000; }
 };
 
 static size_t maxCount = 10;
 
-class UsersList : public JsonArray<UserDto> {
+class UsersList : public JsonArray<UserDto *> {
   public:
 	bool Validate(UserDto *item) override { return Items.size() < maxCount && item->Validate(); }
 };
 
-TEST(JsonArrayTestsGroup, JsonArray_Parse_Test) {
-	UsersList usersList;
+class StringsList : public JsonArray<char *> {
+  public:
+	bool Validate(char *item) override { return Items.size() < maxCount && item != NULL; }
+};
 
-	CHECK(usersList.TryParse("[{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
-							 "\"role\":255}]"));
-	CHECK_EQUAL(usersList.Items.size(), 3);
+// class BoolList : public JsonArray<bool> {
+//  public:
+//	bool Validate(bool item) override { return Items.size() < maxCount; }
+//};
 
-	STRCMP_EQUAL(usersList.Items[0]->Name.Value, "User1");
-	CHECK_EQUAL(usersList.Items[0]->Role.Value, 100);
+class Int64List : public JsonArray<int64_t> {
+  public:
+	bool Validate(int64_t item) override { return Items.size() < maxCount && item < 5188146770730811392LL + 1LL; }
+};
+class Uint64List : public JsonArray<uint64_t> {
+  public:
+	bool Validate(uint64_t item) override { return Items.size() < maxCount && item < 10188146770730811392LL + 1LL; }
+};
+
+class Int32List : public JsonArray<int32_t> {
+  public:
+	bool Validate(int32_t item) override { return Items.size() < maxCount; }
+};
+class Uint32List : public JsonArray<uint32_t> {
+  public:
+	bool Validate(uint32_t item) override { return Items.size() < maxCount; }
+};
+
+class Int16List : public JsonArray<int16_t> {
+  public:
+	bool Validate(int16_t item) override { return Items.size() < maxCount; }
+};
+class Uint16List : public JsonArray<uint16_t> {
+  public:
+	bool Validate(uint16_t item) override { return Items.size() < maxCount; }
+};
+
+class Int8List : public JsonArray<int8_t> {
+  public:
+	bool Validate(int8_t item) override { return Items.size() < maxCount; }
+};
+class Uint8List : public JsonArray<uint8_t> {
+  public:
+	bool Validate(uint8_t item) override { return Items.size() < maxCount; }
+};
+
+TEST(JsonArrayTestsGroup, JsonObjectArray_Parse_Test) {
+	UsersList list;
+
+	CHECK(list.TryParse("[{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
+						"\"role\":255}]"));
+	CHECK_EQUAL(list.Items.size(), 3);
+
+	STRCMP_EQUAL(list.Items[0]->Name.Value, "User1");
+	CHECK_EQUAL(list.Items[0]->Role.Value, 100);
 	return EXIT_SUCCESS;
 }
 
-TEST(JsonArrayTestsGroup, JsonArray_Parse_Error_Test) {
-	UsersList usersList;
-	CHECK_FALSE(usersList.TryParse("[{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
-								   "\"role\":255}"));
-	CHECK_FALSE(usersList.TryParse("{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
-								   "\"role\":255}]"));
-	CHECK_FALSE(usersList.TryParse("[\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
-								   "\"role\":255}]"));
-	CHECK_FALSE(usersList.TryParse("[{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
-								   "\"role\":255]"));
-	CHECK_FALSE(usersList.TryParse("[{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\"\"User3\","
-								   "\"role\":255}]"));
-	CHECK_FALSE(usersList.TryParse("[{\"name\":\"User1\",\"role\":100}{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
-								   "\"role\":255}]"));
-	CHECK_FALSE(usersList.TryParse("[{},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\",\"role\":255}]"));
-	CHECK_FALSE(usersList.TryParse(NULL, 1));
-	CHECK_EQUAL(usersList.Items.size(), 0);
+TEST(JsonArrayTestsGroup, JsonObjectArray_Parse_Error_Test) {
+	UsersList list;
+	CHECK_FALSE(list.TryParse("[{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
+							  "\"role\":255}"));
+	CHECK_FALSE(list.TryParse("{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
+							  "\"role\":255}]"));
+	CHECK_FALSE(list.TryParse("[\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
+							  "\"role\":255}]"));
+	CHECK_FALSE(list.TryParse("[{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
+							  "\"role\":255]"));
+	CHECK_FALSE(list.TryParse("[{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\"\"User3\","
+							  "\"role\":255}]"));
+	CHECK_FALSE(list.TryParse("[{\"name\":\"User1\",\"role\":100}{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
+							  "\"role\":255}]"));
+	CHECK_FALSE(list.TryParse("[{},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\",\"role\":255}]"));
+	CHECK_FALSE(list.TryParse(NULL, 1));
+	CHECK_EQUAL(list.Items.size(), 0);
 
-	CHECK_FALSE(usersList.TryParse("[{\"name\":\"User2\",\"role\":0},null]"));
-	CHECK_EQUAL(usersList.Items.size(), 1);
+	CHECK_FALSE(list.TryParse("[{\"name\":\"User2\",\"role\":0},null]"));
+	CHECK_EQUAL(list.Items.size(), 1);
 	return EXIT_SUCCESS;
 }
 
-TEST(JsonArrayTestsGroup, JsonArray_Parse_With_Begin_End_Stages_Test) {
-	UsersList usersList;
-	auto doc = usersList.BeginTryParse("[{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
-									   "\"role\":255}]");
+TEST(JsonArrayTestsGroup, JsonObjectArray_Parse_With_Begin_End_Stages_Test) {
+	UsersList list;
+	auto doc = list.BeginTryParse("[{\"name\":\"User1\",\"role\":100},{\"name\":\"User2\",\"role\":0},{\"name\":\"User3\","
+								  "\"role\":255}]");
 
 	CHECK(doc != NULL);
-	CHECK_EQUAL(usersList.Items.size(), 3);
+	CHECK_EQUAL(list.Items.size(), 3);
 
-	STRCMP_EQUAL(usersList.Items[0]->Name.Value, "User1");
-	CHECK_EQUAL(usersList.Items[0]->Role.Value, 100);
-	usersList.EndTryParse(doc);
+	STRCMP_EQUAL(list.Items[0]->Name.Value, "User1");
+	CHECK_EQUAL(list.Items[0]->Role.Value, 100);
+	list.EndTryParse(doc);
 	return EXIT_SUCCESS;
 }
 
-TEST(JsonArrayTestsGroup, JsonArray_WriteTo_Test) {
+TEST(JsonArrayTestsGroup, JsonObjectArray_WriteTo_Test) {
 	char buffer[2048];
-	UsersList usersList;
-	usersList.Add(new UserDto("user 1", 0));
-	usersList.Add(new UserDto("user 2", 10));
-	usersList.Add(new UserDto("user 3", 100));
-	usersList.Add(new UserDto("user 4", 1000));
+	UsersList list;
+	list.Add(new UserDto("user 1", 0));
+	list.Add(new UserDto("user 2", 10));
+	list.Add(new UserDto("user 3", 100));
+	list.Add(new UserDto("user 4", 1000));
 
-	CHECK_EQUAL(usersList.WriteToString(buffer, sizeof(buffer)), 115);
-	STRCMP_EQUAL(buffer, "[{\"name\":\"user 1\",\"role\":0},{\"name\":\"user 2\",\"role\":10},{\"name\":\"user 3\",\"role\":100},{\"name\":\"user 4\",\"role\":1000}]");
+	CHECK_EQUAL(list.WriteToString(buffer, sizeof(buffer)), 85);
+	STRCMP_EQUAL(buffer, "[{\"name\":\"user 1\",\"role\":0},{\"name\":\"user 2\",\"role\":10},{\"name\":\"user 3\",\"role\":100}]");
 	return EXIT_SUCCESS;
 }
 
-TEST(JsonArrayTestsGroup, JsonArray_WriteTo_With_Limited_Buffer_Test) {
+TEST(JsonArrayTestsGroup, JsonObjectArray_WriteTo_With_Limited_Buffer_Test) {
 	char buffer[100];
-	UsersList usersList;
-	usersList.Add(new UserDto("user 1", 0));
-	usersList.Add(new UserDto("user 2", 10));
-	usersList.Add(new UserDto("user 3", 100));
-	usersList.Add(new UserDto("user 4", 1000));
+	UsersList list;
+	list.Add(new UserDto("user 1", 0));
+	list.Add(new UserDto("user 2", 10));
+	list.Add(new UserDto("user 3", 100));
+	list.Add(new UserDto("user 4", 999));
 
-	CHECK_EQUAL(usersList.WriteToString(buffer, sizeof(buffer)), 99);
+	CHECK_EQUAL(list.WriteToString(buffer, sizeof(buffer)), 99);
 
 	STRCMP_EQUAL(buffer, "[{\"name\":\"user 1\",\"role\":0},{\"name\":\"user 2\",\"role\":10},{\"name\":\"user 3\",\"role\":100},{\"name\":\"user ");
 	return EXIT_SUCCESS;
@@ -115,27 +163,144 @@ static void OnReady(void *parent, const char *json, int size) {
 	DirectWriteTestBuffer[size] = 0;
 }
 
-TEST(JsonArrayTestsGroup, JsonArray_Direct_Write_From_Json_Memory_Test) {
-	UsersList usersList;
-	usersList.Add(new UserDto("user 1", 0));
-	usersList.Add(new UserDto("user 2", 10));
-	usersList.Add(new UserDto("user 3", 100));
-	usersList.Add(new UserDto("user 4", 1000));
+TEST(JsonArrayTestsGroup, JsonObjectArray_Direct_Write_From_Json_Memory_Test) {
+	UsersList list;
+	list.Add(new UserDto("user 1", 0));
+	list.Add(new UserDto("user 2", 10));
+	list.Add(new UserDto("user 3", 100));
+	list.Add(new UserDto("user 4", 999));
 
-	usersList.DirectWriteTo((void *)987654321, OnReady);
+	list.DirectWriteTo((void *)987654321, OnReady);
 	CHECK_EQUAL(TestParent, (void *)987654321);
-	STRCMP_EQUAL(DirectWriteTestBuffer, "[{\"name\":\"user 1\",\"role\":0},{\"name\":\"user 2\",\"role\":10},{\"name\":\"user 3\",\"role\":100},{\"name\":\"user 4\",\"role\":1000}]");
+	STRCMP_EQUAL(DirectWriteTestBuffer, "[{\"name\":\"user 1\",\"role\":0},{\"name\":\"user 2\",\"role\":10},{\"name\":\"user 3\",\"role\":100},{\"name\":\"user 4\",\"role\":999}]");
 	delete[] DirectWriteTestBuffer;
 	return EXIT_SUCCESS;
 }
 
+TEST(JsonArrayTestsGroup, JsonStringArray_Parse_Test) {
+	StringsList list;
+
+	CHECK(list.TryParse("[\"User1\",\"User2\",\"User3\"]"));
+	CHECK_EQUAL(list.Items.size(), 3);
+
+	STRCMP_EQUAL((char *)list.Items[0], "User1");
+	STRCMP_EQUAL((char *)list.Items[2], "User3");
+	return EXIT_SUCCESS;
+}
+
+TEST(JsonArrayTestsGroup, JsonBoolArray_Parse_Test) {
+	// BoolList list;
+
+	// CHECK(list.TryParse("[true,false,true,false]"));
+	// CHECK_EQUAL(list.Items.size(), 4);
+
+	// CHECK_EQUAL(list.Items[0], true);
+	// CHECK_EQUAL(list.Items[3], false);
+	return EXIT_SUCCESS;
+}
+
+TEST(JsonArrayTestsGroup, JsonInt64Array_Parse_Test) {
+	Int64List list;
+	CHECK(list.TryParse("[0,1,-5188146770730811392,5188146770730811392]"));
+	CHECK_EQUAL(list.Items.size(), 4);
+	CHECK_EQUAL(list.Items[0], 0);
+	CHECK_EQUAL(list.Items[1], 1);
+	CHECK_EQUAL(list.Items[2], -5188146770730811392LL);
+	CHECK_EQUAL(list.Items[3], 5188146770730811392LL);
+	return EXIT_SUCCESS;
+}
+
+TEST(JsonArrayTestsGroup, JsonUint64Array_Parse_Test) {
+	Uint64List list;
+	CHECK(list.TryParse("[1,0,10188146770730811392]"));
+	CHECK_EQUAL(list.Items.size(), 3);
+	CHECK_EQUAL(list.Items[0], 1);
+	CHECK_EQUAL(list.Items[1], 0);
+	CHECK_EQUAL(list.Items[2], 10188146770730811392LL);
+	return EXIT_SUCCESS;
+}
+
+TEST(JsonArrayTestsGroup, JsonInt32Array_Parse_Test) {
+	Int32List list;
+	CHECK(list.TryParse("[0,-2147483647,2147483647]"));
+	CHECK_EQUAL(list.Items.size(), 3);
+	CHECK_EQUAL(list.Items[0], 0);
+	CHECK_EQUAL(list.Items[1], -2147483647);
+	CHECK_EQUAL(list.Items[2], 2147483647);
+	return EXIT_SUCCESS;
+}
+
+TEST(JsonArrayTestsGroup, JsonUint32Array_Parse_Test) {
+	Uint32List list;
+	CHECK(list.TryParse("[0,4294967295]"));
+	CHECK_EQUAL(list.Items.size(), 2);
+	CHECK_EQUAL(list.Items[0], 0);
+	CHECK_EQUAL(list.Items[1], 4294967295);
+	return EXIT_SUCCESS;
+}
+
+TEST(JsonArrayTestsGroup, JsonInt16Array_Parse_Test) {
+	Int16List list;
+	CHECK(list.TryParse("[0,-32768,32767,-2147483647,2147483647]"));
+	CHECK_EQUAL(list.Items.size(), 5);
+	CHECK_EQUAL(list.Items[0], 0);
+	CHECK_EQUAL(list.Items[1], -32768);
+	CHECK_EQUAL(list.Items[2], 32767);
+	CHECK_EQUAL(list.Items[3], 1);
+	CHECK_EQUAL(list.Items[4], -1);
+	return EXIT_SUCCESS;
+}
+
+TEST(JsonArrayTestsGroup, JsonUint16Array_Parse_Test) {
+	Uint16List list;
+	CHECK(list.TryParse("[0,65535,2147483647]"));
+	CHECK_EQUAL(list.Items.size(), 3);
+	CHECK_EQUAL(list.Items[0], 0);
+	CHECK_EQUAL(list.Items[1], 65535);
+	CHECK_EQUAL(list.Items[2], 65535);
+	return EXIT_SUCCESS;
+}
+
+TEST(JsonArrayTestsGroup, JsonInt8Array_Parse_Test) {
+	Int8List list;
+	CHECK(list.TryParse("[0,-128,127,-2147483647,2147483647]"));
+	CHECK_EQUAL(list.Items.size(), 5);
+	CHECK_EQUAL(list.Items[0], 0);
+	CHECK_EQUAL(list.Items[1], -128);
+	CHECK_EQUAL(list.Items[2], 127);
+	CHECK_EQUAL(list.Items[3], 1);
+	CHECK_EQUAL(list.Items[4], -1);
+	return EXIT_SUCCESS;
+}
+
+TEST(JsonArrayTestsGroup, JsonUint8Array_Parse_Test) {
+	Uint8List list;
+	CHECK(list.TryParse("[0,255,65535,2147483647]"));
+	CHECK_EQUAL(list.Items.size(), 4);
+	CHECK_EQUAL(list.Items[0], 0);
+	CHECK_EQUAL(list.Items[1], 255);
+	CHECK_EQUAL(list.Items[2], 255);
+	CHECK_EQUAL(list.Items[3], 255);
+	return EXIT_SUCCESS;
+}
+
 int main(const int argc, const char *argv[]) {
-	TEST_RUN(JsonArrayTestsGroup, JsonArray_Parse_Test);
-	TEST_RUN(JsonArrayTestsGroup, JsonArray_Parse_Error_Test);
-	TEST_RUN(JsonArrayTestsGroup, JsonArray_Parse_With_Begin_End_Stages_Test);
-	TEST_RUN(JsonArrayTestsGroup, JsonArray_WriteTo_Test);
-	TEST_RUN(JsonArrayTestsGroup, JsonArray_WriteTo_With_Limited_Buffer_Test);
-	TEST_RUN(JsonArrayTestsGroup, JsonArray_Direct_Write_From_Json_Memory_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonObjectArray_Parse_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonObjectArray_Parse_Error_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonObjectArray_Parse_With_Begin_End_Stages_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonObjectArray_WriteTo_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonObjectArray_WriteTo_With_Limited_Buffer_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonObjectArray_Direct_Write_From_Json_Memory_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonStringArray_Parse_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonBoolArray_Parse_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonInt64Array_Parse_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonUint64Array_Parse_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonInt32Array_Parse_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonUint32Array_Parse_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonInt16Array_Parse_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonUint16Array_Parse_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonInt8Array_Parse_Test);
+	TEST_RUN(JsonArrayTestsGroup, JsonUint8Array_Parse_Test);
 
 	printf("JsonArrayTestsGroup success");
 	return EXIT_SUCCESS;
