a51e939aff7cebbc22ea16aac1117b052b61faac
Vitaliy Orazov <viordash@mail.ru>
2022-07-24 23:29:00 +0300

    fix compile

--- main.cpp
+++ main.cpp
@@ -6,36 +6,36 @@
 int main(const int argc, const char *argv[]) {
 	fprintf(stdout, "json to class object\n");
 
-	{
-		JsonFieldsContainer container;
-		JsonValue<int, false> intObj(&container, "test", 100);
+	// {
+	// 	JsonFieldsContainer container;
+	// 	JsonValue<int, false> intObj(&container, "test", 100);
 
-		auto res = intObj.TryParse("{\"test\":19}");
-		printf("intObj.TryParse res:%d, val:%d\n", res, (int)intObj.Value);
+	// 	auto res = intObj.TryParse("{\"test\":19}");
+	// 	printf("intObj.TryParse res:%d, val:%d\n", res, (int)intObj.Value);
 
-		rapidjson::Document doc;
-		doc.SetObject();
-		intObj.WriteToDoc(&doc);
-		rapidjson::StringBuffer buffer;
-		rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-		doc.Accept(writer);
-		const char *jsonStr = buffer.GetString();
-	}
+	// 	rapidjson::Document doc;
+	// 	doc.SetObject();
+	// 	intObj.WriteToDoc(&doc);
+	// 	rapidjson::StringBuffer buffer;
+	// 	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+	// 	doc.Accept(writer);
+	// 	const char *jsonStr = buffer.GetString();
+	// }
 
-	{
-		JsonFieldsContainer container;
-		JsonValue<char *, false> strObj(&container, "testStr", "hello json");
-		auto res = strObj.TryParse("{\"testStr\":\"0123456 abcdef\"}");
-		printf("strObj.TryParse res:%d, val:%s\n", res, (char *)strObj.Value);
+	// {
+	// 	JsonFieldsContainer container;
+	// 	JsonValue<char *, false> strObj(&container, "testStr", "hello json");
+	// 	auto res = strObj.TryParse("{\"testStr\":\"0123456 abcdef\"}");
+	// 	printf("strObj.TryParse res:%d, val:%s\n", res, (char *)strObj.Value);
 
-		rapidjson::Document doc;
-		doc.SetObject();
-		strObj.WriteToDoc(&doc);
-		rapidjson::StringBuffer buffer;
-		rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-		doc.Accept(writer);
-		const char *jsonStr = buffer.GetString();
-	}
+	// 	rapidjson::Document doc;
+	// 	doc.SetObject();
+	// 	strObj.WriteToDoc(&doc);
+	// 	rapidjson::StringBuffer buffer;
+	// 	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+	// 	doc.Accept(writer);
+	// 	const char *jsonStr = buffer.GetString();
+	// }
 
 	// JsonStringField<(const char *)"testName", false, 256> testable1("test");
 
